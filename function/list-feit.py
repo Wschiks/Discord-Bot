db079e97c5f7e83af7d68d9ce69326637d2763ef
Lukas-Greef <1209628@student.roc-nijmegen.nl>
2025-11-10 13:30:50 +0100

    random feitje

---
+++ function/list-feit.py
@@ -0,0 +1,7 @@
+feitjes = [
+    "Niek heeft ooit een bot geschreven die alleen 'hoi' zei.",
+    "Niek’s favoriete snack tijdens het coderen is pure logica.",
+    "Niek kan bugs ruiken voordat ze verschijnen.",
+    "Wist je dat Niek een commit deed zonder typo’s? Een legendarisch moment.",
+    "Niek’s favoriete error: ‘expected :, got ;’"
+]
