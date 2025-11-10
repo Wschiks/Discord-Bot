7055907a3ed1444fe74da9d559b95e30b4e5cf25
Lukas-Greef <1209628@student.roc-nijmegen.nl>
2025-11-10 13:30:50 +0100

    random feitje

--- function/feitje.py
+++ function/feitje.py
@@ -1,12 +1,16 @@
 from discord.ext import commands
+import random
+from list-feit import feitjes
 
 class Hello(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="niek")
-    async def hello(self, ctx):
-        await ctx.send("baskldjfhlkasdjfh")
+    async def niek(self, ctx):
+        feitje = random.choice(feitjes)
+        await ctx.send(feitje)
 
 async def setup(bot):
     await bot.add_cog(Hello(bot))
+
